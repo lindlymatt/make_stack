a33caa3ff5d5fbd5c467e8ce14e496f234240f5a
Matthew Lindly <matthew.lindly@hotmail.com>
2017-01-18 09:45:13 -0700

    Finished product, may need to change tests to ES6.

--- stack.js
+++ stack.js
@@ -1,5 +1,60 @@
-function Stack(){
+class Stack() {
+  constructor() {
+    this.top = null;
+    this.count = 0;
+  }
+  // Implement push function.
+  push(val) {
+    let node = { value: val, next: null };
+    node.next = this.top;
+    this.top = node;
+    this.count++;
+  }
+  // Implement pop function.
+  pop() {
+    // Check if there is nothing to pop.
+    if (this.top === null) {
+      return null;
+    }
+    let poppedNode = this.top;
+    this.top = this.top.next;
+    if (this.count > 0) {
+      this.count--;
+    }
+    return poppedNode.value;
+  }
+  // Implement peek function.
+  peek() {
+    if (this.top === null) {
+      return null;
+    } else {
+      return this.top.value;
+    }
+  }
+  // Check if something exists in the stack.
+  search(target) {
+    var node = this.top;
+    if (node.value === target) {
+      return true;
+    }
 
+    while (node.next !== null) {
+      node = node.next;
+      if (node.value === target) {
+        return true;
+      }
+    }
+    return false;
+  }
+  // Implement empty function.
+  empty() {
+    var node = this.top;
+    while (node.next !== null) {
+      this.pop();
+    }
+    this.top = null;
+    this.count = 0;
+  }
 }
 
 module.exports = Stack;
